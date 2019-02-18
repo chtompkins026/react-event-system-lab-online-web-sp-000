35d45bb438fb8be56b6b0f9625174cf95a0f7b97
Christopher Tompkins <chtompkins026@gmail.com>
2019-02-18 19:13:37 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -6,13 +6,13 @@ export default class EyesOnMe extends React.Component{
     console.log('Good!');
   }
 
-  attn = () => {
+  attention = () => {
     console.log('Hey! Eyes on me!');
   }
 
   render() {
     return(
-      <button onFocus={this.focus} onBlur={this.attn}></button>
+      <button onFocus={this.focus} onBlur={this.attention}></button>
     )
   }
 }
